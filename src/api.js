a43e10561f6ae38994f125b956848b6d90dc553d
Turkin Danila <nil.ottoman@gmail.com>
2021-08-30 21:06:55 +0400

    changed http on https in api.js baseURL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://api.football-data.org/v2/',
+  baseURL: 'https://api.football-data.org/v2/',
   headers: { 'X-Auth-Token': process.env.REACT_APP_CODE },
 });
 
